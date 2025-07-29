c0e2126463980392e15ffdc5c119b71818c3d70b
ROKSANA DILSHAD <roksanadilshad@gmail.com>
2025-07-29 20:31:31 +0600

    freedrinks Add

---
+++ free.js
@@ -0,0 +1,4 @@
+let burgerPrice = 650;
+let cokePrice = burgerPrice > 500 ? 0 : 30;
+
+console.log(cokePrice === 0 ? "You get a free Coke!" : "Coke price: " + cokePrice + "tk");
---
+++ freeDrinks.js
@@ -0,0 +1,9 @@
+let burgerPrice = 150; // example burger price
+
+if (burgerPrice > 500) {
+    cokePrice = 0;
+    console.log("You get a free Coke!");
+} else {
+    cokePrice = 30;
+    console.log("Coke price: " + cokePrice + "tk");
+}
